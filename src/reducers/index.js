f535b918d40a8dd32e5da8551238a27443c7b84f
ShehabMuhammad <47852641+ShehabMuhammad@users.noreply.github.com>
2021-11-24 23:34:14 +0200

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux'
 
 import questions from './questions'
-import loading from './loading'
 import users from './users'
 import user from './user'
 import show from './show'
 export default combineReducers({
   questions,
-  loading,
   users,
   user,
   show
